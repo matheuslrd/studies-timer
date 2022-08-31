78d5cabd4a62b650ec2a30f25d9d9a99291d074e
Matheus Laurindo <matheus104521@gmail.com>
2022-08-31 11:06:16 -0300

    feat: (Button) add a type prop

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import style from './botao.module.scss';
 
-class Button extends React.Component<{ children: string }> {
+export type TButton = {
+  children: string;
+  type?: "button" | "submit" | "reset";
+};
+
+class Button extends React.Component<TButton> {
   render() {
-    return <button className={style.botao}>{ this.props.children }</button>;
+    const { type = 'button' } = this.props;
+    return (
+      <button type={type} className={style.botao}>
+        {this.props.children}
+      </button>
+    );
   }
 }
 
