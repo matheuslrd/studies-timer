5b714153f658c11d14fe5c1eaaf55584c9bcdd93
Matheus Laurindo <matheus104521@gmail.com>
2022-08-31 11:06:44 -0300

    feat: (Form) name and time controlled

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -3,16 +3,27 @@ import Button from '../Button';
 import style from './form.module.scss';
 
 class Form extends React.Component {
+  state = {
+    time: '00:00',
+    name: '',
+  };
+
+  adicionarTarefa = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   render(): React.ReactNode {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa}>
         <div className={style.inputContainer}>
           <label>
             Adicione um novo estudo
             <input
               type="text"
-              id="tarefa"
-              name="tarefa"
+              id="name"
+              name="name"
+              value={this.state.name}
+              onChange={({ target }) => this.setState({ ...this.state, name: target.value })}
               placeholder="O que você quer estudar?"
               required
             />
@@ -27,6 +38,8 @@ class Form extends React.Component {
               step="1"
               name="tempo"
               id="tempo"
+              value={this.state.time}
+              onChange={({ target }) => this.setState({ ...this.state, time: target.value })}
               min="00:00:00"
               max="01:30:00"
               required
@@ -34,9 +47,7 @@ class Form extends React.Component {
           </label>
         </div>
 
-        <Button>
-          Adicionar
-        </Button>
+        <Button>Adicionar</Button>
       </form>
     );
   }
