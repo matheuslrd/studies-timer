10440cbf3d0f50b2e159a680de525ee85b8d9258
Matheus Laurindo <matheus104521@gmail.com>
2022-08-31 10:12:29 -0300

    feat: (List) criação de state para a lista

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './list.module.scss';
 import Item from './components/Item';
 
 function List() {
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     {
       name: 'React',
-      time: '02:00:00',
+      time: "02:00:00"
+    },
+    {
+      name: 'Javascript',
+      time: "01:00:00"
     },
     {
       name: 'Typescript',
-      time: '01:00:00',
-    },
-  ];
+      time: "03:00:00"
+    }
+  ]);
 
   return (
     <aside className={style.listaTarefas}>
